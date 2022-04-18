241b9b2c1526dcfeb1c614cb0371a65752781b72
Quang Le <lenhuquang99@gmail.com>
2022-04-18 19:33:14 +0300

    Fix dockerfile, fix frontend import and naming

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,14 +1,14 @@
 import './axios.config.js'
 import { createStore } from 'vuex'
 import auth from './modules/auth.js'
-import profile from './modules/profile.js'
-import post from './modules/post.js'
-import cmt from './modules/cmt.js'
-import reaction from './modules/reaction.js'
-import relationship from './modules/relationship.js'
-import photo from './modules/photo.js'
-import notif from './modules/notif.js'
-import theme from './modules/theme.js'
+// import profile from './modules/profile.js'
+// import post from './modules/post.js'
+// import cmt from './modules/cmt.js'
+// import reaction from './modules/reaction.js'
+// import relationship from './modules/relationship.js'
+// import photo from './modules/photo.js'
+// import notif from './modules/notif.js'
+// import theme from './modules/theme.js'
 
 const store = createStore({
   state: {
