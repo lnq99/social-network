6446f6689a413e33626a6a16119b411ca0cd714b
Jasur Tursunov <ztursunov@ivi.ru>
2022-04-23 23:50:22 +0300

    repo files changed

--- server/internal/repository/repo_profile.go
+++ server/internal/repository/repo_profile.go
@@ -2,96 +2,92 @@ package repository
 
 import (
 	"database/sql"
-	"reflect"
 
 	"app/internal/model"
-	"app/pkg/logger"
 
 	"github.com/lib/pq"
 )
 
-type ProfileRepoImpl struct {
-	DB *sql.DB
+type profileRepoImpl struct {
+	db *sql.DB
 }
 
 func NewProfileRepo(db *sql.DB) ProfileRepo {
-	return &ProfileRepoImpl{db}
+	return &profileRepoImpl{db}
 }
 
-func (r *ProfileRepoImpl) parseProfile(row *sql.Row) (profile model.Profile, err error) {
-	s := reflect.ValueOf(&profile).Elem()
-	numCols := s.NumField()
-	columns := make([]interface{}, numCols)
-	for i := 0; i < numCols; i++ {
-		field := s.Field(i)
-		columns[i] = field.Addr().Interface()
+func scanProfile(row MultiScanner, p *model.Profile) error {
+	return row.Scan(
+		&p.Id,
+		&p.Name,
+		&p.Gender,
+		&p.Birthdate,
+		&p.Email,
+		&p.Phone,
+		&p.Salt,
+		&p.Hash,
+		&p.Created,
+		&p.Intro,
+		&p.AvatarS,
+		&p.AvatarL,
+		&p.PostCount,
+		&p.PhotoCount,
+	)
+}
+
+func (r profileRepoImpl) Insert(p *model.Profile) (id int, err error) {
+	query := `insert into Profile(name, gender, birthdate, email, salt, hash)
+		values ($1, $2, $3, $4, $5, $6) returning id`
+	row := r.db.QueryRow(query, p.Name, p.Gender, p.Birthdate, p.Email, p.Salt, p.Hash)
+	err = row.Scan(&id)
+	return
+}
+
+func (r profileRepoImpl) Select(id int) (p model.Profile, err error) {
+	row := r.db.QueryRow("select * from Profile where id=$1 limit 1", id)
+	err = scanProfile(row, &p)
+	return
+}
+
+func (r profileRepoImpl) SelectByEmail(email string) (p model.Profile, err error) {
+	row := r.db.QueryRow("select * from Profile where email=$1 limit 1", email)
+	err = scanProfile(row, &p)
+	return
+}
+
+func (r profileRepoImpl) UpdateAvatar(photo model.Photo) (err error) {
+	query := `update Profile set avartarL=$1 where id=$2`
+	res, err := r.db.Exec(query, photo.Url, photo.UserId)
+	if err == nil {
+		err = handleRowsAffected(res)
 	}
-	err = row.Scan(columns...)
-
 	return
 }
 
-func (r *ProfileRepoImpl) Select(id int) (model.Profile, error) {
-	row := r.DB.QueryRow("select * from Profile where id=$1 limit 1", id)
-	return r.parseProfile(row)
-}
-
-func (r *ProfileRepoImpl) SelectByEmail(e string) (model.Profile, error) {
-	row := r.DB.QueryRow("select * from Profile where email=$1 limit 1", e)
-	return r.parseProfile(row)
-}
-
-func (r *ProfileRepoImpl) SelectFeed(id, limit, offset int) (feed []int64, err error) {
-	row := r.DB.QueryRow("select feed($1, $2, $3)", id, limit, offset)
-
-	var arr pq.Int64Array
-	err = row.Scan(&arr)
-	feed = arr
-
+func (r profileRepoImpl) UpdateIntro(id int, intro string) (err error) {
+	query := `update Profile set intro=$1 where id=$2`
+	res, err := r.db.Exec(query, intro, id)
+	if err == nil {
+		err = handleRowsAffected(res)
+	}
 	return
 }
 
-func (r *ProfileRepoImpl) SearchName(id int, s string) (res string, err error) {
-	// log.Println(id, s)
+func (r profileRepoImpl) SearchName(id int, s string) (res string, err error) {
 	if len(s) >= 2 {
-		err = r.DB.QueryRow("select search_name($1, $2)", id, s).Scan(&res)
+		err = r.db.QueryRow("select search_name($1, $2)", id, s).Scan(&res)
 	}
 	if err != nil {
-		// log.Println(err)
 		err = nil
 		res = "[]"
 	}
 	return
 }
 
-func (r *ProfileRepoImpl) Insert(p model.Profile) (err error) {
-	query := `insert into Profile(name, gender, birthdate, email, salt, hash)
-	values ($1, $2, $3, $4, $5, $6)`
-	res, err := r.DB.Exec(query, p.Name, p.Gender, p.Birthdate, p.Email, p.Salt, p.Hash)
-	if err == nil {
-		return handleRowsAffected(res)
-	}
-	//logger.Err(err.Error())
-	return
-}
-
-func (r *ProfileRepoImpl) SetAvatar(p model.Photo) (err error) {
-	// avatarS not set for better performance
-	query := `update Profile set avartarL=$1 where id=$2`
-	res, err := r.DB.Exec(query, p.Url, p.UserId)
-	if err == nil {
-		return handleRowsAffected(res)
-	}
-	logger.Err(err.Error())
-	return
-}
-
-func (r *ProfileRepoImpl) ChangeIntro(id int, intro string) (err error) {
-	query := `update Profile set intro=$1 where id=$2`
-	res, err := r.DB.Exec(query, intro, id)
-	if err == nil {
-		return handleRowsAffected(res)
-	}
-	logger.Err(err.Error())
+func (r profileRepoImpl) SelectFeed(id, limit, offset int) (feed []int64, err error) {
+	row := r.db.QueryRow("select feed($1, $2, $3)", id, limit, offset)
+	var arr pq.Int64Array
+	err = row.Scan(&arr)
+	feed = arr
 	return
 }
